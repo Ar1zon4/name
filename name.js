9d01ab6de2b99267d3f81741dded9f5680f51abf
Emmanuel <godlytone@gmail.com>
2022-11-16 08:36:37 +0100

    bro

---
+++ name.js
@@ -0,0 +1,19 @@
+let firstname = "Paul"
+
+if (firstname == "Samuel"){
+    document.getElementById("firstname").innerHTML=(firstname)
+}
+
+if (firstname == "Opeyemi"){
+    document.getElementById("lastname").innerHTML=("love you")
+}
+if (firstname=="Paul"){
+    document.getElementById("firstname").innerHTML=("Invaild")
+}
+
+if (Surname == "Lola"){
+    document.getElementById("middlename").innerHTML("Go good night")
+}
+else{
+    document.getElementById("").innerHTML("Invaild")
+}
